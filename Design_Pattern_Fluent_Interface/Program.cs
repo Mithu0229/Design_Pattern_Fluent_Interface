d2ecf599864a0b34570ac8771ed94e347e50b761
Mithu0229 <showkat131080@gmail.com>
2022-12-12 11:45:01 +0600

    Add project files.

---
+++ Design_Pattern_Fluent_Interface/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+//we can apply multiple properties (or methods) to an object by connecting them with dots (.) without having to re-specify the object name each time
+namespace Design_Pattern_Fluent_Interface
+{
+    public class Employee
+    {
+        public string FullName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Department { get; set; }
+        public string Address { get; set; }
+    }
+    public class FluentEmployee
+    {
+        private Employee employee = new Employee();
+        public FluentEmployee NameOfTheEmployee(string FullName)
+        {
+            employee.FullName = FullName;
+            return this;
+        }
+        public FluentEmployee Born(string DateOfBirth)
+        {
+            employee.DateOfBirth = Convert.ToDateTime(DateOfBirth);
+            return this;
+        }
+        public FluentEmployee WorkingOn(string Department)
+        {
+            employee.Department = Department;
+            return this;
+        }
+        public FluentEmployee StaysAt(string Address)
+        {
+            employee.Address = Address;
+            return this;
+        }
+    }
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            FluentEmployee obj = new FluentEmployee();
+            obj.NameOfTheEmployee("Anurag Mohanty")
+                    .Born("10/10/1992")
+                    .WorkingOn("IT")
+                    .StaysAt("Mumbai-India");
+
+            Console.ReadKey();
+        }
+    }
+}
